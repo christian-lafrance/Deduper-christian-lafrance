934949b7be923ee3dc415b39a79fb5da913827a1
Christian LaFrance <clafranc@talapas-ln1.uoregon.edu>
2022-11-02 11:48:07 -0700

    updated function doc strings to be more descriptive

--- christian-lafrance-deduper.py
+++ christian-lafrance-deduper.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 
+
 import argparse
 import re
+
 
 def get_args():
     parser = argparse.ArgumentParser(description='''
     This script reads in a SAM file containing single-end reads and outputs another SAM file with 
     PCR duplicates removed. It assumes that two reads are duplciates if they are on the same
     strand and chromosome, have the same left-most start position, and have the same UMI. 
-    The input SAM file does not need to be sorted. Assumes a stranded library prep
-    kit was used. 
+    The input SAM file does not need to be sorted. This script stores strings of all UMIs, 
+    chromosomes, strands, and positions seen and stores them in a trie data structure to use less memory. 
+    Assumes a stranded library prep kit was used. 
     ''')
-    parser.add_argument("-f", help="Designates absolute file path to sorted sam file", required=False, type=str)
-    parser.add_argument("-o", help="Designates absolute file path to sorted sam file", required=False, type=str)
-    parser.add_argument("-u", help="Designates file containing the list of UMIs", required=False, type=str)
+    parser.add_argument("-f", help="Designates absolute file path to input sam file", required=True, type=str)
+    parser.add_argument("-o", help="Designates name and file path of sam file with PCR duplicates removed.", required=True, type=str)
+    parser.add_argument("-u", help="Designates file containing the list of expected UMIs", required=True, type=str)
     return parser.parse_args()
 args = get_args()
 
@@ -31,15 +34,15 @@ def read_UMIs(umi_file: str) -> set:
 
 def get_UMI(qname:str) -> str:
     '''
-    Takes in a SAM file line and returns the UMI from the end of the QNAME. 
+    Takes in a QNAME from SAM header and returns the UMI from the end of the QNAME. 
     '''
     return qname.split(":")[-1]
 
 
 def get_strand(flag: str) -> bool:
     '''
-    Takes the current line of the SAM file as an argument and returns a
-    boolean value representing if the current read is the (+) strand. 
+    Takes the the bitwise flag of the current line of the SAM file as an argument 
+    and returns a boolean value representing if the current read is the (+) strand. 
     True means it is the (+) strand and False means it is the (-) strand. 
     Check the bitwise flag (field 2) and if bit 16 is true then the current 
     read is from the (-) strand. 
@@ -52,9 +55,9 @@ def get_strand(flag: str) -> bool:
 
 def get_pos(pos: int, is_plus_strand: bool, cigar: str) -> int:
     '''
-    Takes the current line of the SAM file as an argument, parses the CIGAR 
+    Takes position, strand, and CIGAR string as arguments, parses the CIGAR 
     string and returns the adjusted 1-based left-most start position of the 
-    mapped read. Will also need to call the get_strand function. 
+    mapped read. Returns the adjusted start positin as an int. 
     For (+) strand: pos = mapped_pos + S (beginning of CIGAR)
     For (-) strand: pos = mapped_pos + M's + D's + N's + S(end of CIGAR) - 1
     '''
@@ -83,9 +86,9 @@ def check_trie(trie: dict, tup: tuple) -> bool:
     '''
     Generates a trie data structure as a nested dictionary to prevent
     redundant prefixes from using more memory, such as reads that have
-    the same UMI and strand but have a different position. Returns a 
-    boolean representing if the umi-strand-position combination is 
-    unique. 
+    the same UMI, chromosome, and strand but have a different position. 
+    Returns a boolean representing if the umi-chrom-strand-position combination 
+    is unique. 
     '''
 
     if tup[0] not in trie:
@@ -127,6 +130,9 @@ valid_UMIs = read_UMIs(args.u)
 trie = {}
 counts = {}
 
+'''
+Open input and output same files. 
+'''
 with open(args.o, "w") as sam_out:
     with open(args.f, "r") as sam_in:
         '''
@@ -158,6 +164,8 @@ with open(args.o, "w") as sam_out:
                 else:
                     counts = check_dict(counts, "invalid_umis")
 
-with open("deduper_stats.tsv", "w") as stats:
-    for k, v in sorted(counts.items()):
-        stats.write(f"{k}\t{v}\n")
+
+# This will write a tsv file with all stats per chromosome. 
+# with open("deduper_stats.tsv", "w") as stats:
+#     for k, v in sorted(counts.items()):
+#         stats.write(f"{k}\t{v}\n")
